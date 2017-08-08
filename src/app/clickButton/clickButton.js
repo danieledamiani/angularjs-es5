4ee3793ad71178925ae83a481550a7840e0eabc3
Daniele Damiani <danieledamianiuk@gmail.com>
2017-08-08 10:55:30 +0100

    add styling

--- src/app/clickButton/clickButton.js
+++ src/app/clickButton/clickButton.js
@@ -1,7 +1,9 @@
 var app = angular.module('app');
 
+var buttonTemplate = '<button class="btn btn-default" ng-click="click()">Click me</button>';
+
 app.component('clickButton',  {
-  template: '<button ng-click="click()">Click me</button>',
+  template: buttonTemplate,
   controller: function(Clicks, $scope) {
     $scope.click = function() {
       Clicks.addClick();
--- src/app/title/titleApp.js
+++ src/app/title/titleApp.js
@@ -1,7 +1,7 @@
 var app = angular.module('app');
 
 app.component('titleApp', {
-  template: '<h1>Number of clicks so far: {{clicks()}}</h1>',
+  template: '<span>Number of clicks so far: {{clicks()}}</span>',
   controller: function(Clicks, $scope) {
     $scope.clicks = function() {
       return Clicks.getClicks();
