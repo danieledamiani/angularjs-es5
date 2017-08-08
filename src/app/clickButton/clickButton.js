276f173c12d0e2b332de43a0fb37dba3cfa15eae
Daniele Damiani <danieledamianiuk@gmail.com>
2017-08-08 11:03:04 +0100

    clickButton now sends event

--- src/app/clickButton/clickButton.js
+++ src/app/clickButton/clickButton.js
@@ -1,12 +1,12 @@
 var app = angular.module('app');
 
-var buttonTemplate = '<button class="btn btn-default" ng-click="click()">Click me</button>';
+var buttonTemplate = '<button class="btn btn-default" ng-click="click($event)">Click me</button>';
 
 app.component('clickButton',  {
   template: buttonTemplate,
   controller: function(Clicks, $scope) {
-    $scope.click = function() {
-      Clicks.addClick();
+    $scope.click = function(evt) {
+      Clicks.addClick(evt);
     }
   }
 });
